dc2aeac52a05ea1ea773445774b9aacbaa576765
jigglypop <ydh2244@gmail.com>
2023-06-16 23:20:47 +0900

    0616

---
+++ lecture4/target/debug/deps/lecture4-801ee2a722534358.d
@@ -0,0 +1,5 @@
+/Users/donghwanyeom/Desktop/coding/rust/lecture4/target/debug/deps/lecture4-801ee2a722534358.rmeta: src/main.rs
+
+/Users/donghwanyeom/Desktop/coding/rust/lecture4/target/debug/deps/lecture4-801ee2a722534358.d: src/main.rs
+
+src/main.rs:
